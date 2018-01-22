01743efbb5cc760fdee36311fe89838c07cf4470
Adrymne <jealousy.of.tomorrow@gmail.com>
2018-01-22 16:25:23 +1100

    Add mark hint to Score

--- src/app/Score.js
+++ src/app/Score.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import './Score.css';
-import { getScore, isVsCom } from 'store/reducers';
+import { getScore, isVsCom, getP1Mark, getP2Mark } from 'store/reducers';
 import { P1, P2 } from 'types';
 
-const Score = ({ score, isVsCom }) => (
+const Score = ({ score, isVsCom, p1, p2 }) => (
   <div className="score">
     <div>
-      <b>P1</b>
+      <b>P1</b> <small className="text-muted">{p1}</small>
       <br />
       {score[P1]}
     </div>
@@ -17,7 +17,7 @@ const Score = ({ score, isVsCom }) => (
       {score.tie}
     </div>
     <div>
-      <b>{isVsCom ? 'COM' : 'P2'}</b>
+      <b>{isVsCom ? 'COM' : 'P2'}</b> <small className="text-muted">{p2}</small>
       <br />
       {score[P2]}
     </div>
@@ -26,7 +26,9 @@ const Score = ({ score, isVsCom }) => (
 
 const mapStateToProps = state => ({
   score: getScore(state),
-  isVsCom: isVsCom(state)
+  isVsCom: isVsCom(state),
+  p1: getP1Mark(state),
+  p2: getP2Mark(state)
 });
 
 export default connect(mapStateToProps)(Score);
